ca9d368a0ff6ede0ab09bf96e99d6cd8e58c63d6
bcj <bhargav29feb@gmail.com>
2019-07-15 11:39:42 +0530

    morph-asss

---
+++ js/custom.js
@@ -0,0 +1,144 @@
+
+
+
+
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+   
+        // "singleItem:true" is a shortcut for:
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+   
+    });
+
+    $("#owl-example").owlCarousel({
+        autoPlay: 2000,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            
+            991:{
+                items:2,
+                nav:true,
+                loop:false
+            },
+            1200:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+   
+      });
+
+      $("#owl-example1").owlCarousel({
+        autoPlay: 2000,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            
+            991:{
+                items:2,
+                nav:true,
+                loop:false
+            },
+            1200:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+   
+     
+      });
+      $("#owl-example2").owlCarousel({
+        autoPlay: 2000,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            
+            991:{
+                items:2,
+                nav:true,
+                loop:false
+            },
+            1200:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+   
+     
+      });
+
+      $("#owl-example2").owlCarousel({
+        autoPlay: 2000,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            
+            991:{
+                items:2,
+                nav:true,
+                loop:false
+            },
+            1200:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+   
+   
+      });
+      $("#owl-example3").owlCarousel({
+        autoPlay: 2000,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            
+            991:{
+                items:2,
+                nav:true,
+                loop:false
+            },
+            1200:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+   
+   
+      });
+
+     
+    
+  });
+
+  
+
+  
+  document.getElementById("mobile-trigger").addEventListener("click", function(){
+     $(".custom-menu").slideToggle();
+  })
